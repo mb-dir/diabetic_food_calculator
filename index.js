dd2324b3764ccdd40eafe5bb3202e0f6de0fae2e
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-03-17 09:08:32 +0100

    Create variables based on user input
    Convert it from string to number

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', ()=>{
-    
+    const calcolatorData = document.querySelector('.dataForm');
+
+    calcolatorData.addEventListener('submit', (e)=>{
+        e.preventDefault();
+
+        const ww = parseInt(document.querySelector('#ww').value);//ww = wymiennik węglowodanowy
+        const carbohydrates = parseInt(document.querySelector('#carbohydrates').value);
+        const roughage = parseInt(document.querySelector('#roughage').value);
+    });
 });
